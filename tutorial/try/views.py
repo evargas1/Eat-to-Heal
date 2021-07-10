b2903b62248211d624fbb707aa43038c654e669d
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-07-10 08:33:45 -0500

    fixeed home page lets run

--- tutorial/try/views.py
+++ tutorial/try/views.py
@@ -6,16 +6,20 @@ def index(request):
     return render(request, 'try/index.html', context={})
 
 def aboutus(request):
+    # configure and can see
     return render(request, 'try/about-us.html', context={})
 
 def contact(request):
+    # configure and can see
     return render(request, 'try/contact.html', context={})
 
 def dashboard(request):
     return render(request, 'try/dashboard.html', context={})
 
 def login(request):
+    # configure and can see
     return render(request, 'try/login.html', context={})
 
 def register(request):
+    # configure and can see
     return render(request, 'try/register.html', context={})
