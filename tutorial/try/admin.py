b3ed8794bee957209e67bc3e9a3cf8f77e31f43b
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-07-10 17:30:30 -0500

    registered on admin

--- tutorial/try/admin.py
+++ tutorial/try/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from .models import Contact
-from .forms import SignUpForm
 
 # Register your models here.
 admin.site.register(Contact)
-admin.site.register(SignUpForm)
