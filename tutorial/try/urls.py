e9eb52612204c5605ea5f9e68b016d30b7bb22fd
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-07-10 08:00:55 -0500

    edited the simple pages and testing styles.css

--- tutorial/try/urls.py
+++ tutorial/try/urls.py
@@ -4,10 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('aboutus/', views.index, name='aboutus'),
+    path('aboutus/', views.aboutus, name='aboutus'),
+
     path('contact/', views.contact, name='contact'),
+
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('login/', views.index, name='login'),
+    path('login/', views.login, name='login'),
+
     path('register/', views.register, name='register'),
 
 ]
