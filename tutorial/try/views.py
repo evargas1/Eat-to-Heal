a1c165e3d6146f9f1622adbec4d0883d9697dc5d
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-07-10 08:54:51 -0500

    added the first contact form testing

--- tutorial/try/views.py
+++ tutorial/try/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
+from .models import Contact, ContactForm
 # Create your views here.
 def index(request):
     return render(request, 'try/index.html', context={})
@@ -10,8 +11,24 @@ def aboutus(request):
     return render(request, 'try/about-us.html', context={})
 
 def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            # some sort of action needs to be performed here
+            # (1) save data
+            # (2) send an email ####
+            # (3) return search result
+            # (4) upload a file
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        form = ContactForm()
+
+
+    return render(request, 'try/contact.html', {'form': form})
     # configure and can see
-    return render(request, 'try/contact.html', context={})
+    
 
 def dashboard(request):
     return render(request, 'try/dashboard.html', context={})
