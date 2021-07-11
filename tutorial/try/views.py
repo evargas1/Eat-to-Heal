b732ee8d88c40b635af90ccd35a8b45d42c01512
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-07-11 17:56:24 -0500

    missed typo fixing server

--- tutorial/try/views.py
+++ tutorial/try/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate
 from django.contrib.auth.forms import AuthenticationForm
-from .forms import SignUpForm
+# from .forms import SignUpForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib import messages
