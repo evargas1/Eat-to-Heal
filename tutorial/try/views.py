55d48e4bd347bf1776b91d56391e216914df324a
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-07-11 14:42:51 -0500

    added

--- tutorial/try/views.py
+++ tutorial/try/views.py
@@ -82,7 +82,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=user.username, password=raw_password)
             login(request, user)
-            return redirect('index')
+            return redirect('')
     else:
         form = SignUpForm()
 
