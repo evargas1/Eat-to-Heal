9d603776c844a29ebbd671b6d10f27ed5867d2c8
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-07-12 11:34:14 -0500

    testing contactform

--- tutorial/try/forms.py
+++ tutorial/try/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Contact
+from django.forms import ModelForm
 
 
 class SignUpForm(UserCreationForm):
@@ -29,4 +30,3 @@ class ContactForm(ModelForm):
         model = Contact
         fields = '__all__'
 
-        
